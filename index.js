cf4a6ba8126f4c4ef25e196c8868a8880a4af715
alide123321 <alide12332@gmail.com>
2020-05-24 19:34:45 +0300

    24/7

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const prefix = '.';
 const auther = "alide123321#9518";
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });

fce1fc6ffb9bd1ce64547d0fbb66e5266a95525a
alide123321 <alide12332@gmail.com>
2020-05-26 23:25:36 +0300

    code formating

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ bot.on('ready', () =>
 bot.on('message', msg =>
 {
     if (msg.author.bot || !msg.content.startsWith(prefix)) return; // Ignore the message if it's from a bot or doesn't start with the prefix.
+    
     let args = msg.content.substring(prefix.length).split(" ");
     let text = msg.content;
 

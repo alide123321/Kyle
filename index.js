91893416a38f2dbc7cc897cb49017f0ef3acf4d4
alide123321 <alide12332@gmail.com>
2020-06-06 19:24:56 +0300

    prefix

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ bot.on('message', async msg =>
     
     let args = msg.content.substring(prefix.length).split(" ");
     let text = msg.content;
+
+    
+
     switch(args[0])
     {
         case 'help':{
@@ -104,6 +107,40 @@ bot.on('message', async msg =>
         case 'image':{
             
             break;}
+
+        case 'prefix':{
+            module.exports.run = async (bot, msg, args) => {
+                let prefixes = JSON.parse(fs.readFileSync("./prefixes.json","utf8"));
+                if(!prefixes[msg.guild.id]) {
+                    prefixes[msg.guild.id] = {
+                        prefix: botconfig.prefix
+                    }
+                }
+                let prefix = prefixes[msg.guild.id].prefix;
+            
+                if(!msg.member.hasPermission("MAnage_GUILD")) return msg.channel.send("you do not have the correct permissions");
+                
+                if(!args[0]) return msg.channel.send("please enter a prefix");
+            
+                prefixes[msg.guild.id] = {
+                    prefix: args[0]
+                }
+            
+                fs.writeFile("./prefixes.json",JSON.stringify(prefixes), (err) => {
+                        if (err) console.log(err)
+                });
+            
+                let embed = new Discord.MessageEmbed()
+                    embed.setColor();
+                    embed.setTitle("prefix Set!");
+                    embed.setDescription('set to ${args[0]}');
+            }
+            
+            module.exports.help = {
+                name: "prefix",
+                aliases: []
+            }
+            break}
         
         case '..':
             break;

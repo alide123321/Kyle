95156ecb2d4417f14cc2480f94fbcbf923f2e88d
alide123321 <alide12332@gmail.com>
2020-06-08 11:55:37 +0300

    cant @everyone

--- index.js
+++ index.js
@@ -64,6 +64,9 @@ bot.on('message', msg =>
             break;}
 
         case 'spam':{
+            if(args[1] === "@everyone"){
+                msg.channel.send("no!");
+                break}
             for(var i = 0 ; i !== 5 ; ++i)
                 msg.reply(" said: "+text.slice(5,)+" ");
             break;}

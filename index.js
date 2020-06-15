44281bb7580d91f63d194a040f39e56995ce419a
alide123321 <alide12332@gmail.com>
2020-06-16 02:40:08 +0300

    no defult in switch statment

--- index.js
+++ index.js
@@ -198,10 +198,6 @@ bot.on('message', msg =>
 
         
 
-            default:{
-                msg.channel.send("Sry that is not a command, but it could be \nsend a suggestion here https://forms.gle/98EvJmg7JZ41RDkT9")
-            }
-
             
     }
 

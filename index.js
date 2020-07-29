59cfd851f8f4b1cc9e233b3eaee20c223c935765
alide123321 <alide12332@gmail.com>
2020-07-29 17:32:51 +0300

    woo

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ var help = [
   "*" + prefix + "Simp__________Simp bucks*",
   "*" + prefix + "haram_________Haram*",
   "*" + prefix + "blue__________Scary blue*",
-  "*" + prefix + "our___________Our stuff*"
+  "*" + prefix + "our___________Our stuff*",
+  "*" + prefix + "woo___________Woo back*"
 ];
 
 
@@ -236,6 +237,10 @@ bot.on("message", msg => {
 
     case "blue": {
       msg.channel.send("https://cdn.discordapp.com/attachments/707451317626470455/737791880355381308/blue.jpg");
+    break;}
+
+    case "woo": {
+      msg.channel.send("https://cdn.discordapp.com/attachments/737775095828709508/738086389358264391/woo.gif");
     break;}
   }
 });

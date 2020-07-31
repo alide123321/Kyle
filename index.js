28fde525570da0564b85daf93017b9b24d96b3dc
alide123321 <alide12332@gmail.com>
2020-08-01 00:24:06 +0300

    cooldown

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const fetch = require("node-fetch");
 var opusscript = require("opusscript");
 require('dotenv').config();
 
-
+const talkedRecently = new Set();
 
 
 const token = process.env.TOKEN;
@@ -43,7 +43,19 @@ var help = [
   "*" + prefix + "hamood________Arab*"
 ];
 
+if (talkedRecently.has(msg.author.id)) {
+  msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
+  } else {
 
+  // the user can type the command ... your command code goes here :)
+
+  // Adds the user to the set so that they can't talk for a minute
+  talkedRecently.add(msg.author.id);
+  setTimeout(() => {
+  // Removes the user from the set after a minute
+  talkedRecently.delete(msg.author.id);
+  }, 60000);
+}
 
 bot.once("ready", () => {
   console.log("Ready!");

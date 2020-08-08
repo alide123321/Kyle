de82d0e5922f40f0ea3ae3b642df7fcd9a8e4e13
alide123321 <alide12332@gmail.com>
2020-08-08 06:41:14 +0300

    better announcements

--- index.js
+++ index.js
@@ -402,11 +402,38 @@ bot.on("message", async msg => {
       }
 
       if(!(args[1])){
-        msg.channel.send("you didnt enter the title try again");
+        msg.channel.send("the format for this command is .announce <0/1/2/3> <title>\n0-no one\n1-everyone\n2-gamenight\n3-movienight");
         return;
       }
 
-      let title = text.slice(9);
+      if(!(args[2])){
+        msg.channel.send("the format for this command is .announce <0/1/2/3> <title>\n0-no one\n1-everyone\n2-gamenight\n3-movienight");
+        return;
+      }
+
+      if(isNaN(args[1])){
+        msg.channel.send("the format for this command is .announce <0/1/2/3> <title>\n0-no one\n1-everyone\n2-gamenight\n3-movienight");
+        return;
+      }
+    
+      if(args[1] !== "0" || args[1] !== "1" || args[1] !== "2" || args[1] !== "3"){
+        msg.channel.send("the format for this command is .announce <0/1/2/3> <title>\n0-no one\n1-everyone\n2-gamenight\n3-movienight");
+        return;
+      }
+
+
+      let mention = " ";
+      if(args[1] === "0")
+        mention = "no one";
+      if(args[1] === "1")
+        mention = "@everyone";
+      if(args[1] === "2")
+        mention = "<@&740828344794349658>";
+      if(args[1] === "3")
+        mention = "<@&740828341069676594>";
+      
+
+      let title = text.slice(12);
 
       msg.channel.send("What do you want the discription to be? you have 60 seconds to type it"); 
       let disc = " ";      
@@ -414,13 +441,17 @@ bot.on("message", async msg => {
         {max: 1 , time: 60000}).then(collected => {
           disc = collected.first().content;
 
-          /*let announce = new Discord.MessageEmbed()
+          let announce = new Discord.MessageEmbed()
             .setColor('#0099ff')
             .setTitle("**"+title+"**")
             .setURL("https://discord.gg/hpcxUFy")
             .setDescription(disc)
-            .setThumbnail('https://cdn.discordapp.com/attachments/739019780576641096/739022260857470981/Discord_Rose.png')*/
-          chat.send(disc);
+            .setThumbnail('https://cdn.discordapp.com/attachments/739019780576641096/739022260857470981/Discord_Rose.png')
+
+          if(!(mention === "no one"))
+            chat.send("||"+mention+"||");
+          chat.send(announce);
+          chat.send({files: ["./bar.gif"]});
           
         })
         

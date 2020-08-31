2e784ef5ae3434203b47f8c53770e609a08b05ec
alide123321 <alide12332@gmail.com>
2020-08-31 15:59:01 +0300

    fixed crashing bc of serverstats

--- index.js
+++ index.js
@@ -82,13 +82,17 @@ bot.once("ready", () => {
 });
 
 bot.on('guildMemberAdd', member => {
-  member.guild.channels.cache.get('716939268504813578').send("Welcome to "+member.guild.name+", <@" + member.user.id + ">! To get started, visit <#709238410732240906> and react then go on to <#716212510398873651>. Enjoy your stay! <:goodnight:716209532233318472> <:cheemspray:716217215275237427>"); 
-  serverstats(member);
+  if(member.guild.id === '599061990828277770'){
+    member.guild.channels.cache.get('716939268504813578').send("Welcome to "+member.guild.name+", <@" + member.user.id + ">! To get started, visit <#709238410732240906> and react then go on to <#716212510398873651>. Enjoy your stay! <:goodnight:716209532233318472> <:cheemspray:716217215275237427>"); 
+    serverstats(member);
+  }
 });
 
 
 bot.on("guildMemberRemove", (member) => {
-  serverstats(member)
+  if(member.guild.id === '599061990828277770'){
+    serverstats(member);
+  }
 });
 
 

3fcfb61fc8dd8760f819b265911cf2f5b0ac13c8
alide123321 <alide12332@gmail.com>
2020-09-05 14:45:04 +0300

    added .gn command

--- index.js
+++ index.js
@@ -1660,9 +1660,9 @@ bot.on("message", async msg => {
 
     case "gn": {
       let mentioned = msg.mentions.members.first();
-      if(mentioned === null)
+      if(!mentioned)
         msg.channel.send("https://cdn.discordapp.com/attachments/599061991281131531/751735078203293726/118375543_164947441915565_6934644620562947858_n.mp4")
-      if(mentioned !== null){
+      if(mentioned){
         mentioned.send("**"+msg.author.username+" Says Good Night**")
         mentioned.send("https://cdn.discordapp.com/attachments/599061991281131531/751735078203293726/118375543_164947441915565_6934644620562947858_n.mp4")
         msg.channel.send("Sent "+mentioned.user.username+" a Good Night msg");

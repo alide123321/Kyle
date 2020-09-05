119363c557d3bd75fa5414ae7c3d404ee8a58764
alide123321 <alide12332@gmail.com>
2020-09-05 14:38:00 +0300

    added .gn command

--- index.js
+++ index.js
@@ -72,7 +72,8 @@ var funhelp = [
   "**" + prefix + "wtf___________wtf?**",
   "**" + prefix + "bruh__________that face**",
   "**" + prefix + "pardon________PARDON.**",
-  "**" + prefix + "halal_________absolutely halal**"
+  "**" + prefix + "halal_________absolutely halal**",
+  "**" + prefix + "gn<@>_________Say GN to anyone better if u @ them**"
 ];
 var VChelp = [
   "**" + prefix + "okok__________PopSmoke's OK OK**",
@@ -1655,6 +1656,17 @@ bot.on("message", async msg => {
 
     case "halal": {
       msg.channel.send("https://cdn.discordapp.com/attachments/599061991281131531/751296527103950918/image0.png")
+      break;}
+
+    case "gn": {
+      let mentioned = msg.mentions.members.first();
+      if(mentioned === null)
+        msg.channel.send("https://cdn.discordapp.com/attachments/599061991281131531/751735078203293726/118375543_164947441915565_6934644620562947858_n.mp4")
+      if(mentioned !== null){
+        mentioned.send("**"+msg.author.username+" Says Good Night**")
+        mentioned.send("https://cdn.discordapp.com/attachments/599061991281131531/751735078203293726/118375543_164947441915565_6934644620562947858_n.mp4")
+      }
+
       break;}
     //----- end of memes -----//
 

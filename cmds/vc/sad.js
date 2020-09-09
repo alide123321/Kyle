579609e3fa274171d298eddc021ecf94b8c192c1
alide123321 <alide12332@gmail.com>
2020-09-09 13:34:54 +0300

    made .sad louder

--- cmds/vc/sad.js
+++ cmds/vc/sad.js
@@ -2,7 +2,7 @@ module.exports.run = async (bot, msg, args) => {
     const vc = require('../../functions/vc.js').vc;
     
     var VC = msg.member.voice.channel;
-    vc("sad",1.0,VC,msg.author.id,msg.channel)
+    vc("sad",2.0,VC,msg.author.id,msg.channel)
     msg.channel.send("https://cdn.discordapp.com/attachments/707451317626470455/746467065577078915/video0.mp4");
 }
 

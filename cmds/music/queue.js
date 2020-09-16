7d9ae683b336be0b34e36c97039f64e4052a22ec
Ali A <alide12332@gmail.com>
2020-09-16 06:20:17 +0000

    syncing

--- cmds/music/queue.js
+++ cmds/music/queue.js
@@ -1,5 +1,9 @@
 module.exports.run = async (bot, msg, args) => {
-
+  const queue = require('../../functions/queue.js').queue;
+  const serverQueue = queue.get(msg.guild.id);
+  
+  for(var i = 0; i < serverQueue.songs.length; ++i )
+  msg.channel.send("```"+serverQueue.songs[i]+"```")
 }
 
 module.exports.help = {

35d523f2b9f63caa30ea6d0679392ada76eca95d
alide123321 <alide12332@gmail.com>
2020-09-18 18:41:38 +0300

    added .catch to purge command

--- cmds/mod/purge.js
+++ cmds/mod/purge.js
@@ -5,7 +5,8 @@ module.exports.run = async (bot, msg, args) => {
       return msg.reply("Error please define how many msgs do you want to delete");
     if (args[1] > 100)
       return msg.channel.send("you can only delete 100 messages at a time");
-    msg.channel.bulkDelete(args[1]);
+    msg.channel.bulkDelete(args[1])
+    .catch(console.error);
     }
 }
 

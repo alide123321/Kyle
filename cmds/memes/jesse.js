2914d22acb6d1e3b58ccb2c0d95a93067674c552
alide123321 <alide12332@gmail.com>
2020-09-19 01:51:50 +0300

    added .jesse

---
+++ cmds/memes/jesse.js
@@ -0,0 +1,7 @@
+module.exports.run = async (bot, msg, args) => {
+    msg.channel.send("https://cdn.discordapp.com/attachments/752042865349230662/756648469061697626/Capture.PNG");
+}
+
+module.exports.help = {
+    name: "jesse"
+}

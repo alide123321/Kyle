02526715b5cfc902dc357fd0889b2fe705ee418a
Ali A <alide12332@gmail.com>
2020-09-19 17:40:30 +0000

    .jazzy louder

--- cmds/vc/jazzy.js
+++ cmds/vc/jazzy.js
@@ -2,7 +2,7 @@ module.exports.run = async (bot, msg, args) => {
     const vc = require('../../functions/vc.js').vc;
     
     var VC = msg.member.voice.channel;
-    vc("jazzy",1.0,VC,msg.author.id,msg.channel)
+    vc("jazzy",1.5,VC,msg.author.id,msg.channel)
     msg.channel.send("https://cdn.discordapp.com/attachments/752042865349230662/756065344724140032/video0.mp4");
 }
 

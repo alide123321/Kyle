b1382fea68c14adc7e51ea0dbfe237ba62f7a2b6
Ali A <alide12332@gmail.com>
2020-09-20 06:35:11 +0000

    fixed .jesse

--- cmds/memes/jesse.js
+++ cmds/memes/jesse.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot, msg, args) => {
-    msg.channel.send("https://cdn.discordapp.com/attachments/752042865349230662/757127374637629510/image0.jpg");
+  msg.channel.send("https://cdn.discordapp.com/attachments/752042865349230662/757127374637629510/image0.jpg");
 }
 
 module.exports.help = {
-    name: "jesse"
+  name: "jesse"
 }

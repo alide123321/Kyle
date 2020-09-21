84373eb8af11212ef1e0632cbb66777ffd67e636
alide123321 <alide12332@gmail.com>
2020-09-21 18:42:29 +0300

    added .howgay

---
+++ cmds/memes/howgay.js
@@ -0,0 +1,27 @@
+module.exports.run = async (bot, msg, args) => {
+    let perc = Math.floor(Math.random() * 101);
+    const Discord = require("discord.js");
+    let mentioned = msg.mentions.members.first();
+  
+    if (mentioned) {
+        let menEmbed = new Discord.MessageEmbed()
+            .setTitle("**Using the gay detectior on "+mentioned.user.username+"**")
+            .setColor(0X32CD32)
+            .setThumbnail(mentioned.user.avatarURL())
+            .setDescription(`**${perc}% gay :rainbow_flag:**`)
+        msg.channel.send(menEmbed);
+    return;
+    } else {
+        let authEmbed = new Discord.MessageEmbed()
+            .setTitle("**Using the gay detectior on "+msg.author.username+"**")
+            .setColor(0X32CD32)
+            .setThumbnail(msg.author.avatarURL())
+            .setDescription(`**${perc}% gay :rainbow_flag:**`)
+        msg.channel.send(authEmbed);
+    }
+
+}
+
+module.exports.help = {
+    name: "howgay"
+}

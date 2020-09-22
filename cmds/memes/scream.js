53a7ead1b0edfb120442d8d8a912a893ff6c6bb9
Ali A <alide12332@gmail.com>
2020-09-22 17:21:27 +0000

    added .scream

---
+++ cmds/memes/scream.js
@@ -0,0 +1,7 @@
+module.exports.run = async (bot, msg, args) => {
+    msg.channel.send("https://cdn.discordapp.com/attachments/599061991281131531/758014955311202334/scream.jpg");
+}
+
+module.exports.help = {
+    name: "scream"
+}

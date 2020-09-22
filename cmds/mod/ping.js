5bd3f0abcabcc993594846f28332c08e826606e5
alide123321 <alide12332@gmail.com>
2020-09-22 13:41:56 +0300

    added better ping command

--- cmds/mod/ping.js
+++ cmds/mod/ping.js
@@ -1,5 +1,17 @@
 module.exports.run = async (bot, msg, args) => {
-    msg.channel.send("Im alive");
+    const Discord = require("discord.js");
+    msg.channel.send("Pinging...").then(m =>{
+
+        var ping = m.createdTimestamp - msg.createdTimestamp;
+        var botPing = Math.round(bot.ws.ping);
+
+        var embed = new Discord.MessageEmbed()
+            .setAuthor(`Your ping is ${ping}ms\nBot\'s ping is ${botPing}ms`)
+            .setColor("Your Color")
+          
+        m.delete()
+        msg.channel.send(embed)
+      });
 }
 
 module.exports.help = {

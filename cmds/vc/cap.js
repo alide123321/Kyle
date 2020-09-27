60a99755d84ae50d77d4e591c8513f91a2bc52e3
alide123321 <alide12332@gmail.com>
2020-09-27 18:35:42 +0300

    added .cap

---
+++ cmds/vc/cap.js
@@ -0,0 +1,13 @@
+module.exports.run = async (bot, msg, args) => {
+  const vc = require("../../functions/vc.js").vc;
+
+  var VC = msg.member.voice.channel;
+  vc("cap", 0.5, VC, msg.author.id, msg.channel);
+  msg.channel.send(
+    "https://cdn.discordapp.com/attachments/752042865349230662/759799959229825104/Zias_Stop_The_Cap.mp4"
+  );
+};
+
+module.exports.help = {
+  name: "cap",
+};

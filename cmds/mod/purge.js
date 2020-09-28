0c6f90beae81b7dd2dd136e413e669676dd61743
alide123321 <alide12332@gmail.com>
2020-09-28 16:52:54 +0300

    added negitave number check to .purge

--- cmds/mod/purge.js
+++ cmds/mod/purge.js
@@ -9,11 +9,16 @@ module.exports.run = async (bot, msg, args) => {
         "Error: please define how many messages do you want to delete."
       );
 
+    if (args[1] < 1)
+      return msg.channel.send("Error: please chose a number between 1 and 99");
+
     args[1]++;
 
     if (args[1] > 100)
       return msg.channel.send("You can only delete 99 messages at a time.");
     msg.channel.bulkDelete(args[1]).catch(console.error);
+  } else {
+    return msg.reply("Error: You dont have administrator perms.");
   }
 };
 

2d4e07fdc9f5debcf005cc8656fa86a0c8f56dd0
Ali A <alide12332@gmail.com>
2020-10-07 09:05:28 +0000

    fixed oof

--- cmds/memes/oof.js
+++ cmds/memes/oof.js
@@ -1,5 +1,5 @@
 module.exports.run = async (bot, msg, args) => {
-  let imageNum = Math.floor(Math.random() * 8) + 1;
+  let imageNum = Math.floor(Math.random() * 7) + 1;
   msg.channel.send({ files: ["./assets/images/oof" + imageNum + ".jpg"] });
 };
 

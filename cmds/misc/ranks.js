635eb5201702567fd69d23f291bb167099c61a78
alide123321 <alide12332@gmail.com>
2020-10-07 14:06:09 +0300

    removed not needed code

--- cmds/misc/ranks.js
+++ cmds/misc/ranks.js
@@ -1,20 +1,11 @@
 const Discord = require("discord.js");
 const db = require("quick.db");
 var xp = new db.table("xp");
-let Xp = 0;
-
-const express = require("express");
-const app = express();
-app.use(express.static("public"));
-app.listen(5000, () =>
-  console.log(`Example app listening at http://localhost:${5000}`)
-);
 
 module.exports.run = async (bot, msg, args) => {
   var allusers = (await msg.guild.members.fetch()).keyArray("id");
   var users = [];
   var usersXp = [];
-  var sendarr = [];
 
   for (let x = 0; x <= allusers.length; ++x) {
     if (xp.has(`${msg.guild.id}_${allusers[x]}.xp`)) {

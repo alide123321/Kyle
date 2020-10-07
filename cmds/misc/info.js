646398e8c2183a0a93ffd5cfbbb116d8c75f5b9e
alide123321 <alide12332@gmail.com>
2020-10-07 09:56:12 +0300

    added github link to .info

--- cmds/misc/info.js
+++ cmds/misc/info.js
@@ -1,7 +1,8 @@
 module.exports.run = async (bot, msg, args) => {
   const Discord = require("discord.js");
 
-  const helplink = "https://sites.google.com/view/kyle-bot/home";
+  const helpLink = "https://sites.google.com/view/kyle-bot/home";
+  const codeLink = "https://github.com/alide123321/discordbot";
   const version = process.env.VERSION;
   const prefix = process.env.PREFIX;
   const author = process.env.ALIDE;
@@ -9,7 +10,7 @@ module.exports.run = async (bot, msg, args) => {
   let info = new Discord.MessageEmbed()
     .setColor("#0099ff")
     .setTitle("**Help commands**")
-    .setURL(helplink)
+    .setURL(helpLink)
     .setThumbnail(
       "https://cdn.discordapp.com/attachments/739019780576641096/739022260857470981/Discord_Rose.png"
     )
@@ -17,11 +18,7 @@ module.exports.run = async (bot, msg, args) => {
       { name: "Version: ", value: version, inline: true },
       { name: "Prefix: ", value: prefix, inline: true },
       { name: "Author: ", value: author, inline: true },
-      {
-        name: "Code: ",
-        value: "https://github.com/alide123321/discordbot",
-        inline: true,
-      }
+      { name: "Code: ", value: codeLink, inline: true }
     );
   msg.channel.send(info);
 };

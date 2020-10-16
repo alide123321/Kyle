bab5d426eb4d052b2f93611a15069d5f4b2f4982
alide123321 <alide12332@gmail.com>
2020-10-16 18:23:31 +0300

    sad4 command

---
+++ cmds/memes/sad4.js
@@ -0,0 +1,9 @@
+module.exports.run = async (bot, msg, args) => {
+  msg.channel.send(
+    "https://cdn.discordapp.com/attachments/720556138726162473/766362906475364352/image0.png"
+  );
+};
+
+module.exports.help = {
+  name: "sad4",
+};

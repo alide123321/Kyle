2713a7144bd1589e12a60eabf609711e2d22397d
alide123321 <alide12332@gmail.com>
2020-11-03 12:16:22 +0300

    .ascii

---
+++ cmds/memes/ascii.js
@@ -0,0 +1,43 @@
+module.exports.run = async (bot, msg, args) => {
+  var asciify = require("asciify-image");
+  const fs = require("fs");
+  let link = args[1];
+
+  if (!link) return msg.channel.send("Please give me a link");
+
+  if (!link.endsWith("jpg") && !link.endsWith("gif") && !link.endsWith("png"))
+    return msg.channel.send("unsuported format");
+  var options = {
+    fit: "box",
+    width: 40,
+    height: 40,
+    color: false,
+  };
+  var optionsOut = {
+    fit: "box",
+    width: 350,
+    height: 350,
+    color: false,
+  };
+
+  asciify(link, options, function (err, asciified) {
+    if (err) throw err;
+    msg.channel.send("```" + asciified + "```");
+  });
+
+  asciify(link, optionsOut, function (err, asciified) {
+    if (err) throw err;
+
+    fs.writeFile("./assets/images/HigherQualityAscii.txt", asciified, (err) => {
+      if (err) throw err;
+
+      msg.channel.send("A Higher Quality Version", {
+        files: ["./assets/images/HigherQualityAscii.txt"],
+      });
+    });
+  });
+};
+
+module.exports.help = {
+  name: "ascii",
+};

05752c0c923ccf166f592f61bfc39bd57b115d09
alide123321 <alide12332@gmail.com>
2020-11-08 10:24:43 +0300

    .faisal

---
+++ cmds/memes/faisal.js
@@ -0,0 +1,9 @@
+module.exports.run = async (bot, msg, args) => {
+  msg.channel.send(
+    "https://cdn.discordapp.com/attachments/707451317626470455/774896885033992192/image0.png"
+  );
+};
+
+module.exports.help = {
+  name: "faisal",
+};

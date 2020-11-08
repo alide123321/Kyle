cc0dd1cbbfdd13dff441749ff0fc644824af3679
Ali A <alide12332@gmail.com>
2020-11-08 07:28:38 +0000

    better .faisal

--- cmds/memes/faisal.js
+++ cmds/memes/faisal.js
@@ -1,6 +1,6 @@
 module.exports.run = async (bot, msg, args) => {
   msg.channel.send(
-    "https://cdn.discordapp.com/attachments/707451317626470455/774896885033992192/image0.png"
+    "https://cdn.discordapp.com/attachments/707451317626470455/774897859538714654/Studio_Project.jpeg"
   );
 };
 

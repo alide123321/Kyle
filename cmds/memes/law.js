beef3d9861b36320823b301eadbf944dd4ba11f2
alide123321 <alide12332@gmail.com>
2020-11-17 22:58:37 +0300

    lot of memes

---
+++ cmds/memes/law.js
@@ -0,0 +1,9 @@
+module.exports.run = async (bot, msg, args) => {
+	msg.channel.send(
+		"https://cdn.discordapp.com/attachments/752042865349230662/778347382093119508/videoplayback.mp4"
+	);
+};
+
+module.exports.help = {
+	name: "law",
+};
---
+++ cmds/memes/spin.js
@@ -0,0 +1,9 @@
+module.exports.run = async (bot, msg, args) => {
+	msg.channel.send(
+		"https://cdn.discordapp.com/attachments/727766002036768771/775448400425517066/video0.mp4"
+	);
+};
+
+module.exports.help = {
+	name: "spit",
+};

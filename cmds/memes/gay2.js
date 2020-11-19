ff50d341b285ada87307bba143c06a5e81223483
alide123321 <alide12332@gmail.com>
2020-11-19 11:28:20 +0300

    uwuw

--- cmds/memes/gay2.js
+++ cmds/memes/gay2.js
@@ -1,7 +1,8 @@
 module.exports.run = async (bot, msg, args) => {
-	msg.channel.send(
-		"https://cdn.discordapp.com/attachments/707451317626470455/778898547369836544/video0.mp4"
-	);
+	if (msg.guild.id == "599061990828277770")
+		msg.channel.send(
+			"https://cdn.discordapp.com/attachments/707451317626470455/778898547369836544/video0.mp4"
+		);
 };
 
 module.exports.help = {

1b30ef5e63068edc4a41920f727dc99d36c09069
alide123321 <alide12332@gmail.com>
2020-11-19 11:25:29 +0300

    gay2

---
+++ cmds/memes/gay2.js
@@ -0,0 +1,10 @@
+module.exports.run = async (bot, msg, args) => {
+	msg.channel.send(
+		"https://cdn.discordapp.com/attachments/707451317626470455/778898547369836544/video0.mp4"
+	);
+};
+
+module.exports.help = {
+	name: "gay2",
+	Alias: "amoudi",
+};

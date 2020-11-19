08c43fa9bdc3d1c1d63356e7d920a079266137b2
alide123321 <alide12332@gmail.com>
2020-11-19 10:57:23 +0300

    .sharmota

---
+++ cmds/vc/sharmota.js
@@ -0,0 +1,13 @@
+module.exports.run = async (bot, msg, args) => {
+	const vc = require("../../assets/functions/vc.js").vc;
+
+	var VC = msg.member.voice.channel;
+	vc("sharmota", 1.2, VC, msg.author.id, msg.channel);
+	msg.channel.send(
+		"https://cdn.discordapp.com/attachments/669931327797067807/778891070310318100/videoplayback.mp4"
+	);
+};
+
+module.exports.help = {
+	name: "sharmota",
+};

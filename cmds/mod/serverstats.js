1e1a3b33f21d2d2c5c613a6c0c78b1d0c52fdf1f
alide123321 <alide12332@gmail.com>
2020-11-20 23:43:47 +0300

    serverStats

--- cmds/mod/serverstats.js
+++ cmds/mod/serverstats.js
@@ -1,58 +1,52 @@
 module.exports.run = async (bot, msg, args) => {
-  const Discord = require("discord.js");
-  let Total = msg.guild.memberCount;
-  let bots = msg.guild.members.cache.filter((m) => m.user.bot).size;
-  let users = Total - bots;
-  let online = msg.guild.members.cache.filter((m) => m.status !== "offline").size;
-  let Voice = 0;
-  const voiceChannels = msg.guild.channels.cache.filter((c) => c.type === "voice");
-  for (const [id, voiceChannel] of voiceChannels) Voice += voiceChannel.members.size;
+	const Discord = require("discord.js");
+	let Total = msg.guild.memberCount;
+	let bots = msg.guild.members.cache.filter((m) => m.user.bot).size;
+	let users = Total - bots;
+	let Voice = 0;
+	const voiceChannels = msg.guild.channels.cache.filter((c) => c.type === "voice");
+	for (const [id, voiceChannel] of voiceChannels) Voice += voiceChannel.members.size;
 
-  let Stats = new Discord.MessageEmbed()
-    .setTitle("**Server Stats**")
-    .setColor(0x5fb4f5)
-    .setThumbnail(msg.guild.iconURL())
-    .addFields(
-      {
-        name: "Total Members: ",
-        value: Total,
-        inline: true,
-      },
-      {
-        name: "Users: ",
-        value: users,
-        inline: true,
-      },
-      {
-        name: "Bots: ",
-        value: bots,
-        inline: true,
-      },
-      {
-        name: "Online Users: ",
-        value: `${online - bots} / ${users}`,
-        inline: true,
-      },
-      {
-        name: "In Voice: ",
-        value: Voice,
-        inline: true,
-      },
-      {
-        name: "Owner: ",
-        value: (await msg.guild.members.fetch(msg.guild.owner.id)).user.username,
-        inline: true,
-      }
-    );
+	let Stats = new Discord.MessageEmbed()
+		.setTitle("**Server Stats**")
+		.setColor(0x5fb4f5)
+		.setThumbnail(msg.guild.iconURL())
+		.addFields(
+			{
+				name: "Total Members: ",
+				value: Total,
+				inline: true,
+			},
+			{
+				name: "Users: ",
+				value: users,
+				inline: true,
+			},
+			{
+				name: "Bots: ",
+				value: bots,
+				inline: true,
+			},
+			{
+				name: "In Voice: ",
+				value: Voice,
+				inline: true,
+			},
+			{
+				name: "Owner: ",
+				value: (await msg.guild.members.fetch(msg.guild.owner.id)).user.username,
+				inline: true,
+			}
+		);
 
-  msg.channel.send(Stats);
+	msg.channel.send(Stats);
 
-  if (msg.guild.id !== "599061990828277770") return; //wonderland server
-  msg.guild.channels.cache.get("715444945602740244").setName(`Total Members: ${Total}`); // 770998780404957235
-  msg.guild.channels.cache.get("715444948568244305").setName(`Users: ${Total - bots}`); // 770998825677488178
-  msg.guild.channels.cache.get("715444951332290591").setName(`Bots: ${bots}`); // 770998873937805383
+	if (msg.guild.id !== "599061990828277770") return; //wonderland server
+	msg.guild.channels.cache.get("715444945602740244").setName(`Total Members: ${Total}`); // 770998780404957235
+	msg.guild.channels.cache.get("715444948568244305").setName(`Users: ${Total - bots}`); // 770998825677488178
+	msg.guild.channels.cache.get("715444951332290591").setName(`Bots: ${bots}`); // 770998873937805383
 };
 
 module.exports.help = {
-  name: "serverstats",
+	name: "serverstats",
 };

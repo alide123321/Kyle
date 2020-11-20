8698372f6acd988516c997b2b951f673f54fffdd
alide123321 <alide12332@gmail.com>
2020-11-20 23:05:21 +0300

    fix lackin

--- cmds/memes/lackin.js
+++ cmds/memes/lackin.js
@@ -1,5 +1,5 @@
 module.exports.run = async (bot, msg, args) => {
-	if (args[1] === 2) {
+	if (args[1] === "2") {
 		return msg.channel.send(
 			"https://cdn.discordapp.com/attachments/708429793447772161/779375953255202856/image0.jpg"
 		);

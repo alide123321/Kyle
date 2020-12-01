b6d174867d87a5096c1ca4d1956b7943bbe87407
alide123321 <alide12332@gmail.com>
2020-12-01 23:57:02 +0300

    shadow

---
+++ cmds/memes/shadow.js
@@ -0,0 +1,9 @@
+module.exports.run = async (bot, msg, args) => {
+	msg.channel.send(
+		"https://cdn.discordapp.com/attachments/599061991281131531/783436150551216138/image0.jpg"
+	);
+};
+
+module.exports.help = {
+	name: "shadow",
+};

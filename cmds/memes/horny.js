8c6715b678beb46b34a62e4498f35651498816ab
alide123321 <alide12332@gmail.com>
2020-12-07 00:42:07 +0300

    horrrr

--- cmds/memes/horny.js
+++ cmds/memes/horny.js
@@ -1,6 +1,6 @@
 module.exports.run = async (bot, msg, args) => {
 	msg.channel.send(
-		"https://cdn.discordapp.com/attachments/680928395399266314/785253109408333844/video0.mov"
+		"https://cdn.discordapp.com/attachments/707451317626470455/785259704699584532/video0.mov"
 	);
 };
 

75869c1bf55cab486ffd8ea880ca3228da9a73e9
alide123321 <alide12332@gmail.com>
2020-12-07 00:46:27 +0300

    h

--- cmds/vc/h.js
+++ cmds/vc/h.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot, msg, args) => {
 	const vc = require("../../assets/functions/vc.js").vc;
 
-	vc("h", 0.5, msg);
+	vc("h", 0.3, msg);
 	msg.channel.send(
 		"https://cdn.discordapp.com/attachments/680928395399266314/785252917048377394/video0.mov"
 	);

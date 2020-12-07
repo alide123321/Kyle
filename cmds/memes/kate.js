fd2c3ed6fc232a9ae01177cc3396bfbb4b40682f
alide123321 <alide12332@gmail.com>
2020-12-07 09:27:25 +0300

    kate

---
+++ cmds/memes/kate.js
@@ -0,0 +1,9 @@
+module.exports.run = async (bot, msg, args) => {
+	msg.channel.send(
+		"https://yellowhammernews.com/wp-content/uploads/2017/07/sweet-home-alabama.jpg"
+	);
+};
+
+module.exports.help = {
+	name: "kate",
+};

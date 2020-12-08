d97fa4f02bfdf5271309e928e47b07d8ce80fb15
alide123321 <alide12332@gmail.com>
2020-12-08 15:57:15 +0300

    added server counter

--- assets/handlers/ready.js
+++ assets/handlers/ready.js
@@ -2,9 +2,16 @@ const { bot } = require("../../index");
 require("dotenv").config();
 
 bot.once("ready", () => {
-  console.log("Ready!");
-  console.log("prefix:" + process.env.PREFIX);
-  console.log("version:" + process.env.VERSION);
-  console.log("______________________");
-  bot.user.setActivity("Im also mod mail DM me");
+	let index = 1;
+	console.log("______________________");
+	console.log("Ready!");
+	console.log("prefix:" + process.env.PREFIX);
+	console.log("version:" + process.env.VERSION);
+	console.log("Servers:");
+	bot.guilds.cache.forEach((guild) => {
+		console.log(`   ${index}) ${guild.name}`);
+		++index;
+	});
+	console.log("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾");
+	bot.user.setActivity("Im also mod mail DM me");
 });

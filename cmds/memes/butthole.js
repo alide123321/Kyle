c42ba0bebe0371a919208a1b63dd7894d276bb40
alide123321 <alide12332@gmail.com>
2020-12-10 16:53:22 +0300

    wadawd

---
+++ cmds/memes/butthole.js
@@ -0,0 +1,9 @@
+module.exports.run = async (bot, msg, args) => {
+	msg.channel.send(
+		'https://cdn.discordapp.com/attachments/716215392921452554/786436777816752159/Untitled.png'
+	);
+};
+
+module.exports.help = {
+	name: 'butthole',
+};

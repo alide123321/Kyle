63cb213f8283ac75cc58661033000559ef65e2ca
alide123321 <alide12332@gmail.com>
2020-12-22 23:22:07 +0300

    .sonic

---
+++ cmds/memes/sonic.js
@@ -0,0 +1,9 @@
+module.exports.run = async (bot, msg, args) => {
+	msg.channel.send(
+		'https://cdn.discordapp.com/attachments/752042865349230662/791037734386794526/image0.jpg'
+	);
+};
+
+module.exports.help = {
+	name: 'sonic',
+};

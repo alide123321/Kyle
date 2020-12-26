56abd4ad838fb6640193106226e571993f71e505
alide123321 <alide12332@gmail.com>
2020-12-26 12:32:08 +0300

    bruh2

---
+++ cmds/memes/bruh2.js
@@ -0,0 +1,9 @@
+module.exports.run = async (bot, msg, args) => {
+	msg.channel.send(
+		'https://cdn.discordapp.com/attachments/752042865349230662/792323790042300446/Capture.PNG'
+	);
+};
+
+module.exports.help = {
+	name: 'bruh2',
+};

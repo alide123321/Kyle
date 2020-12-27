b9ade76f166390ce3a554afb844a34f6042e0fa2
alide123321 <alide12332@gmail.com>
2020-12-28 00:47:35 +0300

    .texas

---
+++ cmds/vc/texas.js
@@ -0,0 +1,12 @@
+module.exports.run = async (bot, msg, args) => {
+	const vc = require('../../assets/functions/vc.js').vc;
+
+	vc('texas', 0.7, msg);
+	msg.channel.send(
+		'https://cdn.discordapp.com/attachments/740514553531465769/792871038866030603/Texas.mp4'
+	);
+};
+
+module.exports.help = {
+	name: 'texas',
+};

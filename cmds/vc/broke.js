a46b82a925a4b6cd3c5fa4fda17b93f2874ad909
alide123321 <alide12332@gmail.com>
2020-12-27 20:24:33 +0300

    broke

---
+++ cmds/vc/broke.js
@@ -0,0 +1,12 @@
+module.exports.run = async (bot, msg, args) => {
+	const vc = require('../../assets/functions/vc.js').vc;
+
+	vc('broke', 0.7, msg);
+	msg.channel.send(
+		'https://cdn.discordapp.com/attachments/707451317626470455/792804969954017330/zeaTkam9s9nd1ohs.mp4'
+	);
+};
+
+module.exports.help = {
+	name: 'broke',
+};

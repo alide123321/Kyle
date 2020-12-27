fc4a7865b5e69a0a68c4f49b026e8cfd369ff04b
alide123321 <alide12332@gmail.com>
2020-12-27 23:32:53 +0300

    stfu2

---
+++ cmds/memes/stfu2.js
@@ -0,0 +1,9 @@
+module.exports.run = async (bot, msg, args) => {
+	msg.channel.send(
+		'https://cdn.discordapp.com/attachments/599061991281131531/792852116774977546/video0_1.mp4'
+	);
+};
+
+module.exports.help = {
+	name: 'stfu2',
+};

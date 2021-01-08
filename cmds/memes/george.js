70420f2722a3caf3764f136a13b2259c358f778a
alide123321 <alide12332@gmail.com>
2021-01-09 00:06:18 +0300

    .george

---
+++ cmds/memes/george.js
@@ -0,0 +1,9 @@
+module.exports.run = async (bot, msg, args) => {
+	msg.channel.send(
+		'https://cdn.discordapp.com/attachments/599061991281131531/790149559773954089/979.jpg'
+	);
+};
+
+module.exports.help = {
+	name: 'george',
+};

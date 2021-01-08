a25c5e6e7a044a154775a21df36209b3d559aefb
alide123321 <alide12332@gmail.com>
2021-01-08 16:52:33 +0300

    pizza

---
+++ cmds/vc/pizza.js
@@ -0,0 +1,12 @@
+module.exports.run = async (bot, msg, args) => {
+	const vc = require('../../assets/functions/vc.js').vc;
+
+	vc('pizza', 0.7, msg);
+	msg.channel.send(
+		'https://cdn.discordapp.com/attachments/741866598289375342/796457981159538738/image0.jpg'
+	);
+};
+
+module.exports.help = {
+	name: 'pizza',
+};

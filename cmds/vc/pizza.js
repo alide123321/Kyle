d6b02d1a227f6820a101c285340d01cbf94f8c30
alide123321 <alide12332@gmail.com>
2021-01-08 16:57:31 +0300

    shorter pizza

--- cmds/vc/pizza.js
+++ cmds/vc/pizza.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot, msg, args) => {
 	const vc = require('../../assets/functions/vc.js').vc;
 
-	vc('pizza', 0.7, msg);
+	vc('pizza', 0.5, msg);
 	msg.channel.send(
 		'https://cdn.discordapp.com/attachments/741866598289375342/796457981159538738/image0.jpg'
 	);

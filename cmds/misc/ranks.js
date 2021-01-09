6d62bb6769a024a7dcd1457d38f33c412b8170bd
alide123321 <alide12332@gmail.com>
2021-01-10 01:49:46 +0300

    rank

--- cmds/misc/ranks.js
+++ cmds/misc/ranks.js
@@ -1,7 +1,5 @@
 module.exports.run = async (bot, msg, args) => {
-	msg.channel.send(
-		"Wonderland's leaderboard: https://arcanebot.xyz/leaderboard/599061990828277770"
-	);
+	msg.channel.send("Wonderland's leaderboard: https://arcanebot.xyz/lb/Wonderland");
 };
 
 module.exports.help = {

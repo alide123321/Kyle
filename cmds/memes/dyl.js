cb1d9e5fea043967dba2fc4ecddf0671397eee73
alide123321 <alide12332@gmail.com>
2021-01-12 19:29:38 +0300

    .dyl

---
+++ cmds/memes/dyl.js
@@ -0,0 +1,11 @@
+module.exports.run = async (bot, msg, args) => {
+	msg.channel
+		.send('https://cdn.discordapp.com/attachments/707451317626470455/798586756441440266/ali.mp4')
+		.then((Smsg) => {
+			Smsg.react('😭');
+		});
+};
+
+module.exports.help = {
+	name: 'dyl',
+};

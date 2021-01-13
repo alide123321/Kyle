b0f112f53dc6edc4549c85781bc8410ad9cd8c9b
alide123321 <alide12332@gmail.com>
2021-01-14 02:46:44 +0300

    bruh moment

--- cmds/music/lyrics.js
+++ cmds/music/lyrics.js
@@ -2,6 +2,9 @@
  * Code For music is in KyleMusic bot*
  * Code For music is in KyleMusic bot*
  *************************************/
+module.exports.run = async (bot, msg, args) => {
+	return;
+};
 module.exports.help = {
 	name: 'lyrics',
 	Alias: 'ly',

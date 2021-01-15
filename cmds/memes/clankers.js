bd7377b0dbb4d71ec7af861bd63c3cafe7a5ada0
alide123321 <alide12332@gmail.com>
2021-01-15 23:44:30 +0300

    .clankers

---
+++ cmds/memes/clankers.js
@@ -0,0 +1,9 @@
+module.exports.run = async (bot, msg, args) => {
+	msg.channel.send(
+		'https://cdn.discordapp.com/attachments/798733509727879198/799368974511898685/clankers.mp4'
+	);
+};
+
+module.exports.help = {
+	name: 'clankers',
+};

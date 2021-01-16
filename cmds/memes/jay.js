3d81c97223cdfcea149a548b343b133518de39db
alide123321 <alide12332@gmail.com>
2021-01-16 03:47:17 +0300

    jay

--- cmds/memes/jay.js
+++ cmds/memes/jay.js
@@ -1,6 +1,6 @@
 module.exports.run = async (bot, msg, args) => {
 	msg.channel.send(
-		'"I fliped and lost 10 times in a row bot not worky" \nhttps://tenor.com/view/clash-of-clans-hog-rider-minecraft-pig-hog-gif-18667885'
+		'https://tenor.com/view/clash-of-clans-hog-rider-minecraft-pig-hog-gif-18667885'
 	);
 };
 

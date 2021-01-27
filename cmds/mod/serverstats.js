480968e740515aad4894bbaa7502182d04d92f46
alide123321 <alide12332@gmail.com>
2021-01-27 16:39:18 +0300

    bruh

--- cmds/mod/serverstats.js
+++ cmds/mod/serverstats.js
@@ -1,44 +1,129 @@
 module.exports.run = async (bot, msg, args) => {
+	const moment = require('moment');
 	try {
 		const Discord = require('discord.js');
 		let Total = await msg.guild.memberCount;
 		let bots = await msg.guild.members.cache.filter((m) => m.user.bot).size;
-		let users = Total - bots;
+		let icon, splash;
 		let Voice = 0;
 		const voiceChannels = await msg.guild.channels.cache.filter((c) => c.type === 'voice');
 		for (const [id, voiceChannel] of voiceChannels) Voice += voiceChannel.members.size;
 
-		let Stats = new Discord.MessageEmbed()
-			.setTitle('**Server Stats**')
-			.setColor(0x5fb4f5)
-			.setThumbnail(msg.guild.iconURL())
-			.addFields(
+		let Stats = new Discord.MessageEmbed().setTitle('**Server Stats**').setColor(0x5fb4f5);
+
+		if (msg.guild.iconURL) {
+			icon = `[Click](${msg.guild.iconURL()})`;
+			Stats.setThumbnail(msg.guild.iconURL());
+		} else icon = 'None';
+
+		if (msg.guild.splash) {
+			let splashURL = `https://cdn.discordapp.com/icons/${guild.id}/${guild.splash}.png?size=4096`;
+			splash = `[Click](${splashURL})`;
+			Stats.setImage(splashURL);
+		} else splash = 'None';
+
+		if (args[1] > 0) {
+			Stats.addFields(
 				{
-					name: 'Total Members: ',
-					value: Total,
+					name: 'Name',
+					value: msg.guild.name,
 					inline: true,
 				},
 				{
-					name: 'Users: ',
-					value: users,
+					name: 'ID',
+					value: msg.guild.id,
 					inline: true,
 				},
 				{
-					name: 'Bots: ',
-					value: bots,
+					name: 'Owner',
+					value: msg.guild.owner,
 					inline: true,
 				},
 				{
-					name: 'In Voice: ',
-					value: Voice,
+					name: 'Created on',
+					value: moment(msg.guild.createdTimestamp).format('M/D/YY h:mm:ss A'),
 					inline: true,
 				},
 				{
-					name: 'Owner: ',
-					value: `<@${msg.guild.ownerID}>`,
+					name: 'AFK Timeout',
+					value: msg.guild.afkChannel ? msg.guild.afkTimeout : 'None',
+					inline: true,
+				},
+				{
+					name: 'Region',
+					value: msg.guild.region,
+					inline: true,
+				},
+				{
+					name: 'Icon',
+					value: icon,
+					inline: true,
+				},
+				{
+					name: 'Splash',
+					value: splash,
 					inline: true,
 				}
 			);
+		}
+
+		if (args[1] > 1) {
+			Stats.addFields(
+				{
+					name: 'Verification',
+					value: msg.guild.verificationLevel,
+					inline: true,
+				},
+				{
+					name: '2FA Requirement',
+					value: msg.guild.mfaLevel ? 'On' : 'Off',
+					inline: true,
+				},
+				{
+					name: 'Content Filter',
+					value: msg.guild.explicitContentFilter,
+					inline: true,
+				}
+			);
+		}
+
+		Stats.addFields(
+			{
+				name: 'Channels',
+				value: msg.guild.channels.cache.size,
+				inline: true,
+			},
+			{
+				name: 'In Voice: ',
+				value: Voice,
+				inline: true,
+			},
+			{
+				name: 'Emojis',
+				value: msg.guild.emojis.cache.size,
+				inline: true,
+			},
+			{
+				name: 'Roles',
+				value: msg.guild.roles.cache.size,
+				inline: true,
+			},
+			{
+				name: 'Members',
+				value: Total - bots,
+				inline: true,
+			},
+			{
+				name: 'Bots',
+				value: bots,
+				inline: true,
+			},
+			{
+				name: 'Total users',
+				value: Total,
+				inline: true,
+			}
+		);
 
 		msg.channel.send(Stats);
 

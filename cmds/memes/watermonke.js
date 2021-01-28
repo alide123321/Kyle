eced11c1e2fcaf3ffe819a517c78ebe033df64bf
alide123321 <alide12332@gmail.com>
2021-01-28 21:07:20 +0300

    watermonke

---
+++ cmds/memes/watermonke.js
@@ -0,0 +1,10 @@
+module.exports.run = async (bot, msg, args) => {
+	msg.channel.send(
+		'https://tenor.com/view/yes-moment-moment-monke-moment-monkey-monki-gif-18818008'
+	);
+};
+
+module.exports.help = {
+	name: 'watermonke',
+	description: 'Monke moment',
+};

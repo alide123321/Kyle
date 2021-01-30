dfe3c40acecf09b390456267d8946d599dfda412
alide123321 <alide12332@gmail.com>
2021-01-30 13:31:04 +0300

    .dababy vc command

--- cmds/vc/dababy.js
+++ cmds/vc/dababy.js
@@ -1,10 +1,13 @@
 module.exports.run = async (bot, msg, args) => {
+	const vc = require('../../assets/functions/vc.js').vc;
+
+	vc('dababy', 0.7, msg);
 	msg.channel.send(
 		'https://cdn.discordapp.com/attachments/752042865349230662/785763872543211541/dababy.mp4'
 	);
 };
 
 module.exports.help = {
-	name: 'dababy',
-	description: 'Convertiable',
+	name: 'Dababy',
+	description: 'Dababy convertiable',
 };

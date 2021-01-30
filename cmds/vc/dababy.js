41b3aec2ba90bcf80a5100c7d874972e582ece3d
alide123321 <alide12332@gmail.com>
2021-01-30 13:35:55 +0300

    lower volume

--- cmds/vc/dababy.js
+++ cmds/vc/dababy.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot, msg, args) => {
 	const vc = require('../../assets/functions/vc.js').vc;
 
-	vc('dababy', 0.7, msg);
+	vc('dababy', 0.5, msg);
 	msg.channel.send(
 		'https://cdn.discordapp.com/attachments/752042865349230662/785763872543211541/dababy.mp4'
 	);

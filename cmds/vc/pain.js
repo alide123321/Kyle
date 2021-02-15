bb9dc8404b0751b399a0f49da76f0473ec0c66e2
alide123321 <alide12332@gmail.com>
2021-02-15 23:13:10 +0300

    pain is vc command

--- cmds/vc/pain.js
+++ cmds/vc/pain.js
@@ -1,4 +1,7 @@
 module.exports.run = async (bot, msg, args) => {
+	const vc = require('../../assets/functions/vc.js').vc;
+
+	vc('okok', 0.5, msg);
 	msg.channel.send(
 		'https://cdn.discordapp.com/attachments/707451317626470455/810907346338971648/image0.jpg'
 	);

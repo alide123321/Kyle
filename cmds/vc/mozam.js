54496d59875412cf10ba74a1e8e167362fa26ed6
alide123321 <alide12332@gmail.com>
2021-03-08 11:33:56 +0300

    .mozan

---
+++ cmds/vc/mozam.js
@@ -0,0 +1,13 @@
+module.exports.run = async (bot, msg, args) => {
+	const vc = require('../../assets/functions/vc.js').vc;
+
+	vc('mozam', 0.7, msg);
+	msg.channel.send(
+		'https://cdn.discordapp.com/attachments/752042865349230662/818400963508830248/mozambique_here_voice_lifeline.mp4'
+	);
+};
+
+module.exports.help = {
+	name: 'mozam',
+	description: 'Mozambique HERE',
+};

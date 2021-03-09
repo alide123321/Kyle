419bd001b20553c23272ff3724aedbe08010e7aa
alide123321 <alide12332@gmail.com>
2021-03-09 22:39:50 +0300

    .mimir

---
+++ cmds/memes/mimir.js
@@ -0,0 +1,11 @@
+module.exports.run = async (bot, msg, args) => {
+	msg.channel.send(
+		'https://cdn.discordapp.com/attachments/752042865349230662/797591796372865084/video0_5.mp4'
+	);
+};
+
+module.exports.help = {
+	name: 'mimir',
+	alias: 'amimir',
+	description: 'Go a mimir',
+};

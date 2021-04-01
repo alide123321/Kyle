d9dfc766b8ba9f976447e8969712ba9ded26b86e
alide123321 <alide12332@gmail.com>
2021-04-01 10:04:50 +0300

    you can mention

--- cmds/misc/avatar.js
+++ cmds/misc/avatar.js
@@ -1,9 +1,15 @@
 module.exports.run = async (bot, msg, args) => {
 	const Discord = require('discord.js');
+	let mentioned = msg.mentions.members.first();
+	let Username = mentioned
+		? `${mentioned.user.username}#${mentioned.user.discriminator}`
+		: `${msg.author.username}#${msg.author.discriminator}`;
+	let AvatarUrl = mentioned ? mentioned.user.avatarURL() : msg.author.avatarURL();
+
 	let avatarEmbed = new Discord.MessageEmbed()
-		.setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.avatarURL())
+		.setAuthor(Username, AvatarUrl)
 		.setTitle('**Avatar**')
-		.setImage(msg.author.avatarURL());
+		.setImage(AvatarUrl);
 	msg.channel.send(avatarEmbed);
 };
 

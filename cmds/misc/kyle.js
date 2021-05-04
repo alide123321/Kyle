8e1a1ca89bd9a66155021d125c64239bbdab7ced
alide123321 <alide12332@gmail.com>
2021-05-05 02:38:26 +0300

    fixed .kyle

--- cmds/misc/kyle.js
+++ cmds/misc/kyle.js
@@ -1,48 +1,20 @@
 require('dotenv').config();
-var fs = require('fs');
-const ChatBotObj = JSON.parse(fs.readFileSync('assets/util/ChatBot.json'));
-const Derieri = require('derieri');
-const timeout = 180000; //3 min
-const deri = new Derieri.Client({
-	islearning: true,
-});
+const fetch = require('node-fetch').default;
 
 module.exports.run = async (bot, msg, args) => {
 	let text = msg.content.substring(process.env.PREFIX.length + 5);
-	if (!args[1]) return msg.channel.send('What?');
+	if (!text) return msg.channel.send('Sup?');
 
-	if (!ChatBotObj[msg.author.id]) {
-		deri.reply(text).then((response) => {
-			msg.channel.send(response);
+	fetch(`https://api.snowflakedev.xyz/api/chatbot?message=${encodeURIComponent(text)}&name=Kyle`, {
+		headers: { Authorization: process.env.SnowFlakeToken },
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			msg.channel.send(data.message);
+		})
+		.catch((e) => {
+			return msg.channel.send(`**ERROR** ${e}`);
 		});
-		ChatBotObj[msg.author.id] = {
-			Previous: [text],
-			LM: new Date().getTime(),
-		};
-		fs.writeFileSync('assets/util/ChatBot.json', JSON.stringify(ChatBotObj));
-	} else {
-		if (timeout - (Date.now() - ChatBotObj[msg.author.id].LM) <= 0) {
-			deri.reply(text).then((response) => {
-				msg.channel.send(response);
-			});
-			ChatBotObj[msg.author.id] = {
-				Previous: [text],
-				LM: new Date().getTime(),
-			};
-			return fs.writeFileSync('assets/util/ChatBot.json', JSON.stringify(ChatBotObj));
-		}
-		let PreviousArr = ChatBotObj[msg.author.id].Previous;
-
-		deri.reply(text, PreviousArr).then((response) => {
-			msg.channel.send(response);
-		});
-		PreviousArr.push(text);
-		ChatBotObj[msg.author.id] = {
-			Previous: PreviousArr,
-			LM: new Date().getTime(),
-		};
-		fs.writeFileSync('assets/util/ChatBot.json', JSON.stringify(ChatBotObj));
-	}
 };
 
 module.exports.help = {

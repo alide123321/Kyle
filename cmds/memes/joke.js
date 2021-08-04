9272eb99d246848222c0ad85dfa9ed1565c7c846
alide123321 <alide12332@gmail.com>
2021-08-04 13:38:45 +0300

    I rly leaked the API key dam (changed key)

--- cmds/memes/joke.js
+++ cmds/memes/joke.js
@@ -1,11 +1,10 @@
 module.exports.run = async (bot, msg, args) => {
-	const Discord = require('discord.js');
 	var unirest = require('unirest');
 
 	var req = unirest('GET', 'https://manatee-jokes.p.rapidapi.com/manatees/random');
 
 	req.headers({
-		'x-rapidapi-key': '73d301f3eemshb03e814d13d59d4p105050jsn9c11ec6d87e2',
+		'x-rapidapi-key': process.env.RAPIDAPI,
 		'x-rapidapi-host': 'manatee-jokes.p.rapidapi.com',
 		useQueryString: true,
 	});

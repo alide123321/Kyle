c3e96c125572e041de37b358b9c99b1079641af3
alide123321 <alide12332@gmail.com>
2021-08-22 14:49:24 +0300

    camel

---
+++ cmds/memes/camel.js
@@ -0,0 +1,8 @@
+module.exports.run = async (bot, msg, args) => {
+	msg.channel.send('https://tenor.com/view/camel-arab-arabia-arabic-nike-gif-21428992');
+};
+
+module.exports.help = {
+	name: 'camel',
+	description: 'Camel stay drippy',
+};

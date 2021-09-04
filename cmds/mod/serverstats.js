f06421acec94f8b593c769ca387621233448ae5e
alide123321 <alide12332@gmail.com>
2021-09-04 11:28:39 +0300

    forgot about this

--- cmds/mod/serverstats.js
+++ cmds/mod/serverstats.js
@@ -128,9 +128,7 @@ module.exports.run = async (bot, msg, args) => {
 		msg.channel.send(Stats);
 
 		if (msg.guild.id !== '599061990828277770') return; //wonderland server
-		msg.guild.channels.cache.get('715444945602740244').setName(`Total Members: ${Total}`); // 770998780404957235
-		msg.guild.channels.cache.get('715444948568244305').setName(`Users: ${Total - bots}`); // 770998825677488178
-		msg.guild.channels.cache.get('715444951332290591').setName(`Bots: ${bots}`); // 770998873937805383
+		msg.guild.channels.cache.get('715444945602740244').setName(`Members: ${Total}`); // 770998780404957235
 	} catch (error) {
 		return console.log(error);
 	}
